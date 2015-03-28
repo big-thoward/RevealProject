6a9c7768ee04efc3a4d8c89145fc9d06695cfda0
Trisha Howard <thoward@impactgroup.us>
2015-03-28 08:56:30 -0500

    Initial Commit

---
+++ app/js/controllers/categoryCtrl.js
@@ -0,0 +1,72 @@
+four51.app.controller('CategoryCtrl', ['$rootScope', '$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
+function ($rootScope, $routeParams, $sce, $scope, $451, Category, Product, Nav) {
+	$scope.thetitle = "Projects";
+	$scope.productLoadingIndicator = true;
+	$scope.settings = {
+		currentPage: 1,
+		pageSize: 40
+	};
+			
+	$scope.trusted = function(d){
+		if(d) return $sce.trustAsHtml(d);
+	}
+
+	function _search() {
+		$scope.searchLoading = true;
+		Product.search($routeParams.categoryInteropID, null, null, function (products, count) {
+			$scope.products = products;
+			$scope.productCount = count;
+			$scope.productLoadingIndicator = false;
+			$scope.searchLoading = false;
+		}, $scope.settings.currentPage, $scope.settings.pageSize);
+	}
+
+	$scope.$watch('settings.currentPage', function(n, o) {
+		if (n != o || (n == 1 && o == 1))
+			_search();
+	});
+
+	if ($routeParams.categoryInteropID) {
+	    $scope.categoryLoadingIndicator = true;
+        Category.get($routeParams.categoryInteropID, function(cat) {
+            $scope.currentCategory = cat;
+	        $scope.categoryLoadingIndicator = false;
+        });
+        $rootScope.categoryInteropID = $routeParams.categoryInteropID;
+    }
+	else if($scope.tree){
+		$scope.currentCategory ={SubCategories:$scope.tree};
+	}
+
+
+	$scope.$on("treeComplete", function(data){
+		if (!$routeParams.categoryInteropID) {
+			$scope.currentCategory ={SubCategories:$scope.tree};
+		}
+	});
+
+    // panel-nav
+    $scope.navStatus = Nav.status;
+    $scope.toggleNav = Nav.toggle;
+	$scope.$watch('sort', function(s) {
+		if (!s) return;
+		(s.indexOf('Price') > -1) ?
+			$scope.sorter = 'StandardPriceSchedule.PriceBreaks[0].Price' :
+			$scope.sorter = s.replace(' DESC', "");
+		$scope.direction = s.indexOf('DESC') > -1;
+	});
+
+	$scope.setSubCat = function(c)
+	{
+		$rootScope.subCategoryInteropID = c.InteropID;
+		$routeParams.subCategoryInteropID = c.InteropID;
+		if(!c.InteropID)
+		{
+			$rootScope.subCategoryInteropID = $scope.currentCategory.Description;
+			$routeParams.subCategoryInteropID = $scope.currentCategory.Description;
+			
+		}
+	};
+	
+
+}]);
---
+++ app/js/controllers/checkOutViewCtrl.js
@@ -0,0 +1,157 @@
+four51.app.controller('CheckOutViewCtrl', ['$rootScope', '$scope', '$routeParams', '$location', '$filter', '$rootScope', '$451', 'Analytics', 'User', 'Order', 'OrderConfig', 'FavoriteOrder', 'AddressList',
+function ($rootScope, $scope, $routeParams, $location, $filter, $rootScope, $451, Analytics, User, Order, OrderConfig, FavoriteOrder, AddressList) {
+	$scope.errorSection = 'open';
+	$scope.shipping = true;
+
+	if($scope.user.Type == "TempCustomer")
+	{
+		$rootScope.$broadcast('guest');
+	    $rootScope.guest = true;
+	}
+	
+	$scope.isEditforApproval = $routeParams.id != null && $scope.user.Permissions.contains('EditApprovalOrder');
+	if ($scope.isEditforApproval) {
+		Order.get($routeParams.id, function(order) {
+			$scope.currentOrder = order;
+		});
+	}
+
+	if (!$scope.currentOrder) {
+        $location.path('projects');
+    }
+
+	$scope.hasOrderConfig = OrderConfig.hasConfig($scope.currentOrder, $scope.user);
+	$scope.checkOutSection = $scope.hasOrderConfig ? 'order' : 'shipping';
+
+    function submitOrder() {
+	    $scope.displayLoadingIndicator = true;
+	    $scope.errorMessage = null;
+	    $rootScope.$broadcast('guest');
+	    $rootScope.guest = true;
+        Order.submit($scope.currentOrder,
+	        function(data) {
+//				if ($scope.user.Company.GoogleAnalyticsCode) {
+//					Analytics.trackOrder(data, $scope.user);
+//				}
+				$scope.user.CurrentOrderID = null;
+				User.save($scope.user, function(data) {
+			        $scope.user = data;
+	                $scope.displayLoadingIndicator = false;
+		        });
+		        $scope.currentOrder = null;
+		        $location.path('/order/' + data.ID);
+	        },
+	        function(ex) {
+		        $scope.errorMessage = ex.Message;
+		        $scope.displayLoadingIndicator = false;
+		        $scope.shippingUpdatingIndicator = false;
+		        $scope.shippingFetchIndicator = false;
+	        }
+        );
+    };
+
+	$scope.$watch('currentOrder.CostCenter', function() {
+		OrderConfig.address($scope.currentOrder, $scope.user);
+	});
+
+    function saveChanges(callback) {
+	    $scope.displayLoadingIndicator = true;
+	    $scope.errorMessage = null;
+	    $scope.actionMessage = null;
+	    var auto = $scope.currentOrder.autoID;
+	    Order.save($scope.currentOrder,
+	        function(data) {
+		        $scope.currentOrder = data;
+		        if (auto) {
+			        $scope.currentOrder.autoID = true;
+			        $scope.currentOrder.ExternalID = 'auto';
+		        }
+		        $scope.displayLoadingIndicator = false;
+		        if (callback) callback($scope.currentOrder);
+	            $scope.actionMessage = "Your changes have been saved";
+	        },
+	        function(ex) {
+		        $scope.currentOrder.ExternalID = null;
+		        $scope.errorMessage = ex.Message;
+		        $scope.displayLoadingIndicator = false;
+		        $scope.shippingUpdatingIndicator = false;
+		        $scope.shippingFetchIndicator = false;
+	        }
+        );
+    };
+
+    $scope.continueShopping = function() {
+    	$rootScope.$broadcast('guest');
+	    $rootScope.guest = true;
+	    if (confirm('Do you want to save changes to your order before continuing?') == true)
+	    {
+	    	
+	        saveChanges(function() { $location.path('campaigns') });
+	    }
+        else
+        {
+		    $location.path('campaigns');
+        }
+    };
+
+    $scope.cancelOrder = function() {
+	    if (confirm('Are you sure you wish to cancel your order?') == true) {
+		    $scope.displayLoadingIndicator = true;
+		    $rootScope.$broadcast('guest');
+	    	$rootScope.guest = true;
+	        Order.delete($scope.currentOrder,
+		        function() {
+		            $scope.user.CurrentOrderID = null;
+		            $scope.currentOrder = null;
+			        User.save($scope.user, function(data) {
+				        $scope.user = data;
+				        $scope.displayLoadingIndicator = false;
+				        $location.path('campaigns');
+			        });
+
+		        },
+		        function(ex) {
+			        $scope.actionMessage = ex.Message;
+			        $scope.displayLoadingIndicator = false;
+		        }
+	        );
+	    }
+    };
+
+    $scope.saveChanges = function() {
+        saveChanges();
+    };
+
+    $scope.submitOrder = function() {
+       submitOrder();
+    };
+
+    $scope.saveFavorite = function() {
+        FavoriteOrder.save($scope.currentOrder);
+    };
+
+	$scope.cancelEdit = function() {
+		$location.path('order');
+		$rootScope.$broadcast('guest');
+	    $rootScope.guest = true;
+	};
+
+	$scope.orderToggle = function() {
+		$scope.order = !$scope.order;
+	};
+
+	$scope.billingToggle = function() {
+		$scope.billing = !$scope.billing;
+	};
+
+	$scope.shippingToggle = function() {
+		$scope.shipping = !$scope.shipping;
+	};
+
+	$scope.shippinasbilling = function()
+	{
+		$scope.shipaddress.IsBilling = true;
+		$scope.currentOrder.copyShipAddress = true;
+		$rootScope.$broadcast('shipChange');
+	};
+}]);
