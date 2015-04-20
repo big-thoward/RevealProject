58d4096e8a063e946a75337d1f31899f0edeaf22
Trisha W Howard <thoward@impactgroup.us>
2015-04-20 10:45:22 -0500

    related product view update

--- app/js/controllers/relatedProductsCtrl.js
+++ app/js/controllers/relatedProductsCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 four51.app.controller('RelatedProductsCtrl', ['$rootScope', '$scope', 'Product', '$sce', function($rootScope, $scope, Product, $sce){
 
 	$scope.categoryInteropID = $rootScope.categoryInteropID;
@@ -35,15 +34,4 @@ four51.app.controller('RelatedProductsCtrl', ['$rootScope', '$scope', 'Product',
 		$scope.fixedurl = fixedurl;
 		$scope.theurl = theurl;
 	}
-=======
-four51.app.controller('RelatedProductsCtrl', ['$scope', 'Product', '$sce', function($scope, Product, $sce){
-	if($scope.relatedgroupid){
-		Product.search(null, null, $scope.relatedgroupid, function(products) {
-			$scope.relatedProducts = products;
-		});
-		$scope.trusted = function(d){
-			if(d) return $sce.trustAsHtml(d);
-		}
-	}
->>>>>>> upstream/master
 }]);
