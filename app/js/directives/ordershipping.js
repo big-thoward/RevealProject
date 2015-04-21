98f05b5b25d55d882d0080e7b452ccb1baa0042c
Trisha W Howard <thoward@impactgroup.us>
2015-04-21 10:14:24 -0500

    shiplockdown 333

--- app/js/directives/ordershipping.js
+++ app/js/directives/ordershipping.js
@@ -37,16 +37,15 @@ four51.app.directive('ordershipping', ['$rootScope', 'Order', 'Shipper', 'Addres
 				$scope.shipaddress = { Country: 'US', IsShipping: true, IsBilling: true };
 				if($scope.shipaddress.Country == "US")
 				{
-					$scope.shipid="WztWj9oMRXBSYbKk-pM0nug-e-e";
-					alert($scope.shipid);
+					$scope.currentOrder.LineItems[0].shipid="WztWj9oMRXBSYbKk-pM0nug-e-e";
 				}					
 				else if($scope.shipaddress.Country == "CA")
 				{
-					angular.extend($scope.shipaddress,{shipid:"m35UNNRNJALjz9YIIr6DmQ-e-e"});
+					$scope.currentOrder.LineItems[0].shipid="m35UNNRNJALjz9YIIr6DmQ-e-e";
 				}
 				else
 				{
-					angular.extend($scope.shipaddress,{shipid:"YHTJNi-smVo0DXDluTeXb3w-e-e"});
+					$scope.currentOrder.LineItems[0].shipid="YHTJNi-smVo0DXDluTeXb3w-e-e";
 				}
 			});
 
