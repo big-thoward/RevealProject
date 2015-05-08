d18e0d679d9b4e03a5ee3390110ff70964af3988
Trisha W Howard <thoward@impactgroup.us>
2015-05-08 08:39:34 -0500

    FAQ 1 7

--- app/js/controllers/contactCtrl.js
+++ app/js/controllers/contactCtrl.js
@@ -1,6 +1,6 @@
 four51.app.controller('ContactCtrl', ['$rootScope', '$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
 function ($rootScope, $routeParams, $sce, $scope, $451, Category, Product, Nav) {
-
+	$scope.collapseanswers = true;
 	$scope.reveal = function(divid) {
 		$scope.collapseanswers = true;
 		$scope.+divid = true;
