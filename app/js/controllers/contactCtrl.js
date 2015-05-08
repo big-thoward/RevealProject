ec0f9f3f42a0dddec52f3338ae12e2f5e18c52d6
Trisha W Howard <thoward@impactgroup.us>
2015-05-08 09:06:49 -0500

    FAQ 1 10

--- app/js/controllers/contactCtrl.js
+++ app/js/controllers/contactCtrl.js
@@ -1,30 +1,7 @@
 four51.app.controller('ContactCtrl', .'$rootScope', '$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
 function ($rootScope, $routeParams, $sce, $scope, $451, Category, Product, Nav) {
-	$scope.reveal = function(divid) {
-		$scope.answer.faq0 = null;
-		$scope.answer.faq1 = null;
-		$scope.answer.faq2 = null;
-		$scope.answer.faq3 = null;
-		$scope.answer.faq4 = null;
-		$scope.answer.faq5 = null;
-		$scope.answer.faq6 = null;
-		$scope.answer.faq7 = null;
-		$scope.answer.faq8 = null;
-		$scope.answer.faq9 = null;
-		$scope.answer.faq0 = null;
-		$scope.answer.faq11 = null;
-		$scope.answer.faq12 = null;
-		$scope.answer.faq13 = null;
-		$scope.answer.faq14 = null;
-		$scope.answer.faq15 = null;
-		$scope.answer.faq16 = null;
-		$scope.answer.faq17 = null;
-		$scope.answer.faq18 = null;
-		$scope.answer.faq19 = null;
-		$scope.answer.faq20 = null;
-		$scope.answer.faq21 = null;
-		var dividint = parseInt(divid,10);
-		var divindex = dividint - 1;
-		$scope.answer.faqdivindex = true;
+	$scope.reveal = function() {
+		$scope.answershow = false;
+		$scope.answer1 = true;
     };
 });
