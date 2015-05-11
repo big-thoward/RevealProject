5a7a596675bd2f5582ad1bd0bd9fb203534ebd9c
Trisha W Howard <thoward@impactgroup.us>
2015-05-11 08:36:10 -0500

    address name 3

--- app/js/controllers/addressInputCtrl.js
+++ app/js/controllers/addressInputCtrl.js
@@ -3,7 +3,6 @@ function ($scope, $rootScope, $location, User, Address, Resources) {
     $scope.guest = true;
     $scope.save = function() {
 	    $scope.objectExists = false;
-        $scope.address.AddressName = "TEMP ADDRESS";
         if($scope.user.Type == "Customer")
         {
             Address.save(this.address,
