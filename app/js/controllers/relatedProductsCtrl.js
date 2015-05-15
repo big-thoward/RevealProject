7c54c9d1e876ccc134e568c21cd317359ea654dc
Trisha W Howard <thoward@impactgroup.us>
2015-05-15 08:01:39 -0500

    Related Product Update

--- app/js/controllers/relatedProductsCtrl.js
+++ app/js/controllers/relatedProductsCtrl.js
@@ -9,23 +9,10 @@ four51.app.controller('RelatedProductsCtrl', ['$rootScope', '$scope', 'Product',
 			$scope.relatedProducts = products;
 			var numrelated = products.length;
 		    var liwidth = 100/numrelated;
-		   	licss = "width:"+liwidth+"%";
-		    $scope.liwidth = licss;
+		   	var licss = "width:"+liwidth+"%";
+		   	if(licss){ $scope.liwidth = licss;}
+		   
 		});
-
-		(function(d, s, id) {
-	      var js, fjs = d.getElementsByTagName(s)[0];
-	      if (d.getElementById(id)) return;
-	      js = d.createElement(s); js.id = id;
-	      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-	      fjs.parentNode.insertBefore(js, fjs);
-	    }(document, 'script', 'facebook-jssdk'));
-
-		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-		$scope.trusted = function(d)
-		{
-			if(d) return $sce.trustAsHtml(d);
-		}
 	}
 
 	$scope.changeProduct = function(){
