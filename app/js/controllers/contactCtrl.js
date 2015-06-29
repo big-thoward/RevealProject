bf4d85f7799f071856ad7660407620f85c466667
Trisha Howard <thoward@impactgroup.us>
2015-06-29 11:55:31 -0500

    Updates files to include online overrides

--- app/js/controllers/contactCtrl.js
+++ app/js/controllers/contactCtrl.js
@@ -1,16 +1,12 @@
 four51.app.controller('ContactCtrl', ['$rootScope', '$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
-function ($rootScope, $routeParams, $sce, $scope, $451, Category, Product, Nav) {
-	$scope.faq = '0';
-	$scope.togglefaq = function(id) {
-		if($scope.faq == id)
-		{
-			$scope.faq = '0';
-		}
-		else
-		{
-			$scope.faq = id;
-		}
-		
-	};
-}]);
-
+    function($rootScope, $routeParams, $sce, $scope, $451, Category, Product, Nav) {
+        $scope.faq = '0';
+        $scope.togglefaq = function(id) {
+            if ($scope.faq == id) {
+                $scope.faq = '0';
+            } else {
+                $scope.faq = id;
+            }
+        };
+    }
+]);
